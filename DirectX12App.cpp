5ad1e5a94b2024b87d64f39f341fe9aa437426a5
Adi Ibn Sahr <adi_ibnsahr13@outlook.com>
2024-12-01 12:44:41 +0700

    Setup Project

--- DirectX12App.cpp
+++ DirectX12App.cpp
@@ -3,9 +3,12 @@
 
 #include <iostream>
 
+#include "WinInclude.h"
+#include "ComPointer.h"
+
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
